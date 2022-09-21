b492b469718314434b69cdc6295ffb9b6a274a72
Zane <zskateray@gmail.com>
2022-09-21 16:36:36 -0400

    StreamCom

--- bash.js
+++ bash.js
@@ -1,1 +1,14 @@
 console.log("hola")
+
+//Output a prompt
+process.stdout.write('prompt > ');
+
+// The stdin 'data' event fires after a user types in a line
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim(); //remove the newline
+  
+  if (cmd === "pwd"){
+        process.stdout.write(`Your current directory: ${process.cwd()}`);
+    }
+  process.stdout.write('\nprompt > ');
+});
